82da40accdaa0070146344b30e9cbbbe0e0a4c3a
Sierra Lopez <sierralopez1006@gmail.com>
2022-10-12 21:32:23 -0700

    unfinished. commented out code that needs to be fixed

--- index.js
+++ index.js
@@ -21,8 +21,31 @@ const myMap = {
       .addTo(this.map)
       .bindPopup('<p1><b>You are here</b><br></p1>')
       .openPopup()
-  }
 
+      // new myMap.markers({
+      //   getPosition(),
+      //   map,
+      //   title: "Coffee",
+      // });
+
+      // new myMap.markers({
+      //   getPosition(),
+      //   map,
+      //   title: "Restaurant",
+      // });
+
+      // new myMap.markers({
+      //   getPosition(),
+      //   map,
+      //   title: "Hotel",
+      // });
+
+      // new myMap.markers({
+      //   getPosition(),
+      //   map,
+      //   title: "Market",
+      // });
+  }
 }
 
 async function getCoords() {
@@ -33,14 +56,31 @@ async function getCoords() {
 
 }
 
-//foursquare stuff
+//foursquare stuff below
+//my api key: fsq3rmVAQNxS0HBbAWTJGnslK/0S+Ln8twtXIJF4iUAneJw=
+
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'fsq3rmVAQNxS0HBbAWTJGnslK/0S+Ln8twtXIJF4iUAneJw='
+  }
+}
+
+async function getPosition(){
+  const response = await fetch('https://cors-anywhere.herokuapp.com/https://api.foursquare.com/v3/places/search?query=' + `${business}` + '&ll=' + `${pos.coords.latitude}` + '%2C' + `${pos.coords.longitude}` + '&limit=5', options)
+  var result = await response.text();
+  return [result.latitude, result.longitude];
+}
 
 window.onload = async () => {
   const coords = await getCoords()
   console.log(coords)
   myMap.coordinates = coords
+  // const position = await getPosition()
+  // console.log(position)
+  // myMap.businesses = position
   myMap.buildMap()
-
 }
 
 document.getElementById('submit').addEventListener('click', async (event) => {
